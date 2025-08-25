f40a31a82e2a4a9e5665e90d1d7d8cb6fe8d7d1d
ManojNathIC <manoj_n@tekditechnologies.com>
2025-08-25 16:45:44 +0530

    Task #000: Feat: Added Azure blob storage support for media

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-storage",
+      providerOptions: {
+        authType: "default",
+        account: env("AZURE_STORAGE_ACCOUNT_NAME"),
+        accountKey: env("AZURE_STORAGE_ACCOUNT_KEY"),
+        serviceBaseURL: env("AZURE_STORAGE_URL"),
+        containerName: env("AZURE_STORAGE_CONTAINER_NAME", "public"),
+        defaultPath: env("AZURE_STORAGE_DEFAULT_PATH", "uploads"),
+        maxConcurrent: 10,
+      },
+    },
+  },
+});
